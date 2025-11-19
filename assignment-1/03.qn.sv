522a9ff85374ab7e8afacc8a53dc025ba667f24e
Vishalpalanisamy <vishal.nkl04@gmail.com>
2025-11-19 23:21:35 +0530

    Create 03.qn.sv

---
+++ assignment-1/03.qn.sv
@@ -0,0 +1,7 @@
+//Why can’t we use a foreach loop for popping all the elements in a queue?  
+
+For each is read only modify panna kudathu 
+
+Pop panna queue size change aagum, so for each confuse agum and it  lead to an error 
+
+Instead we can use the while loop
