ba5eef90014d64cb938de396831dd4890a97cca4
Vishalpalanisamy <vishal.nkl04@gmail.com>
2025-12-24 16:25:11 +0530

    Document enum variable behavior in 04.qn.sv
    Add explanation about enum variable behavior when using next method.

---
+++ assignment-1/04.qn.sv
@@ -0,0 +1,3 @@
+//What happens when an enum variable is assigned with the last valid value and the next method is used to do the next assignment?
+
+answer : when the value is already a last valid  but u call next then it show the (x) unknown
